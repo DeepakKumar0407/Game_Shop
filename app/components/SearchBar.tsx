f5e2745130769f764e87727f3f47aab01ec5d3b9
Deepak Kumar <deepak.kumar016211@gmail.com>
2026-01-06 16:57:45 +0530

    add error and not found page and search validation

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -8,10 +8,12 @@ const SearchBar = () => {
     const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
         setSearch(e.target.value)
     }
- 
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+      e.preventDefault()
+    }
   return (
     <div>
-        <form className='flex justify-baseline gap-1'>
+        <form className='flex justify-baseline gap-1' onSubmit={handleSubmit}>
             <input type="text" name="search" value={search} placeholder="search" onChange={handleChange}required></input>
             <Link href={search===''?"":`http://localhost:3000/search/${search}`} ><MagnifyingGlassIcon className='w-5 h-5'/></Link>
         </form>
---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+const error = () => {
+  return (
+    <div>
+        <h1 className="md:text-xl lg:text-2xl text-white">
+            Something went wrong
+        </h1>
+    </div>
+  )
+}
+export default error
